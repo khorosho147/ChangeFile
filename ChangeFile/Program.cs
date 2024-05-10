0a6c77daf00c829d272476a3fbed001f29da5c6e
khorosho147 <qq875598749@gmail.com>
2024-05-10 18:14:19 +0900

    Implement modification of file name

--- ChangeFile/Program.cs
+++ ChangeFile/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +11,44 @@ namespace ChangeFile
     {
         static void Main(string[] args)
         {
-            string path = ""
+            //这里修改要修改文件的目录
+            string path = "C:\\0cda1209\\测试";
+            //获取这个路径下的所有文件
+            DirectoryInfo root = new DirectoryInfo(path);
+            FileInfo[] files = root.GetFiles();
+            List<FileInfo> listFiles = files.ToList();
+
+
+            for(int i = 0; i < listFiles.Count; i++)
+            {
+                string fileName = listFiles[i].Name;
+                
+
+                bool isHave = fileName.Contains("123");
+                if (isHave)
+                {
+                    //给文件改名字
+                    string srcFileName = listFiles[i].FullName;
+                    string dstFileName = listFiles[i].Directory.FullName+"/ddd" + listFiles[i].Extension;
+
+                    File.Move(srcFileName, dstFileName);
+                }
+                Console.WriteLine(fileName);
+                Console.WriteLine(isHave);
+
+                //if (listFiles[i].Name == "新建 文本文档.txt")
+                //{
+                //    System.IO.File.Delete(listFiles[i].FullName);
+                //    Console.WriteLine("新建 文本文档.txt已经删除");
+                //}
+
+                //Console.WriteLine(listFiles[i].FullName);
+                //Console.WriteLine(listFiles[i].Name);
+            }
+
+
+            Console.ReadKey();
+
         }
     }
 }
